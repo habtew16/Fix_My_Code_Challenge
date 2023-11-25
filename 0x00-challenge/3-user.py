09635a0b92d9e7156f93168465ea16994c876921
Habtamu <habtamuwolde96@gmail.com>
2023-11-25 11:43:24 -0800

    fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,7 +52,7 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        elif self.__password is None:
             return False
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
